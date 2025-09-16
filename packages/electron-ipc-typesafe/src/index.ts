c8dcae6cce2ac006379f067b86d54a2ddbb4a42f
ccpu <m.doaie@hotmail.com>
2025-09-16 18:02:19 +0100

    feat: deleted exports for `create-ipc-events` and `create-ipc-handlers`
    * Streamlines the module's public API.

--- packages/electron-ipc-typesafe/src/index.ts
+++ packages/electron-ipc-typesafe/src/index.ts
@@ -1,5 +1,3 @@
-export * from './create-ipc-events';
-export * from './create-ipc-handlers';
 export * from './create-ipc-schema';
 export * from './define-event';
 export * from './define-handler';
